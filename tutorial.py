45296fa419aeb85eacb4e764aae3c1bcde581875
Lionel De Swarte--Danko <64731565+double-dash-dino@users.noreply.github.com>
2021-06-23 13:43:32 +0100

    add more to the tutorial

--- tutorial.py
+++ tutorial.py
@@ -2,3 +2,9 @@
 
 pip install keras
 pip install tensorflow
+
+# Run the imports
+from keras.datasets.cifar10 import load_data
+from matplotlib import pyplot
+
+(trainX, trainy), (textX, testy) = load_data
